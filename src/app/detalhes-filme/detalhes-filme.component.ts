27f3c53db3caab2bee4068f16c1f91cfa647c2e1
Guilherme <guii_arantes@hotmail.com>
2022-12-29 17:16:14 -0300

    alteracoes imagem missing

--- src/app/detalhes-filme/detalhes-filme.component.ts
+++ src/app/detalhes-filme/detalhes-filme.component.ts
@@ -16,67 +16,74 @@ export class DetalhesFilmeComponent implements OnInit {
   videos!: any[];
   detalhes!: any;
   imgBaseUrl = environment.urlImgBase;
-  trailerBaseUrl = 'https://www.youtube.com/watch?v=';
+
   videoUrl!: any;
-  filmeId:any;
-  genres:any;
+  filmeId: any;
+  genres: any;
   casts!: any[];
-  releaseDate!:any[];
+  releaseDate!: any[];
 
 
+  constructor(
+    private filmesService: FilmesService,
+    private sanitizer: DomSanitizer,
+    private route: ActivatedRoute
 
-  constructor(private filmesService: FilmesService, private sanitizer: DomSanitizer, private route: ActivatedRoute) {
-    this.route.params.subscribe(params => this.filmeId = params['id']);
+  ) {
+    this.route.params.subscribe((params) => (this.filmeId = params['id']));
   }
 
   ngOnInit(): void {
     this.filmesService.getDetails(this.filmeId).subscribe((response: any) => {
-      console.log(response);
+
       this.detalhes = response;
-      this.genres = this.detalhes.genres.map((genre:any)=> genre.name).join(", ")
+      this.genres = this.detalhes.genres
+        .map((genre: any) => genre.name)
+        .join(', ');
     });
 
-    this.filmesService.getRecomendations(this.filmeId).subscribe((response: any) => {
-      console.log(response);
-      this.recomendacoes = response.results;
-    });
+    this.filmesService
+      .getRecomendations(this.filmeId)
+      .subscribe((response: any) => {
+
+        this.recomendacoes = response.results;
+      });
 
     this.filmesService.getCredits(this.filmeId).subscribe((response: any) => {
-      console.log(response);
+
       this.credits = response.cast;
       this.casts = response.crew;
+      console.log(this.casts);
+
     });
 
     this.filmesService.getVideo(this.filmeId).subscribe((response: any) => {
-      console.log(response);
+
       this.videos = response.results;
-      this.updateVideoUrl(this.videos[0].key)
+      this.updateVideoUrl(this.videos[0].key);
     });
 
-    this.filmesService.getReleaseDate(this.filmeId).subscribe((response: any) => {
-      console.log(response);
-      this.releaseDate = response.results;
-    });
+    this.filmesService
+      .getReleaseDate(this.filmeId)
+      .subscribe((response: any) => {
 
-
-
+        this.releaseDate = response.results;
+        console.log(this.releaseDate)
+      });
   }
 
   updateVideoUrl(key: string) {
-    // Appending an ID to a YouTube URL is safe.
-    // Always make sure to construct SafeValue objects as
-    // close as possible to the input data so
-    // that it's easier to check if the value is safe.
     const dangerousVideoUrl = 'https://www.youtube.com/embed/' + key;
-    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(dangerousVideoUrl);
+    this.videoUrl =
+      this.sanitizer.bypassSecurityTrustResourceUrl(dangerousVideoUrl);
   }
-
 
   converterHora(value: number): string {
     let hours = Math.floor(value / 60);
     let minutes = Math.floor(value % 60);
     return hours + 'h ' + minutes + 'm';
   }
+
 
 
 }
